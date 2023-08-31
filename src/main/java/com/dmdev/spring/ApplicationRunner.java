03514ebb604bd95c27a5d4d7869b1b381be35201
ramilhanov <sa-msk@yandex.ru>
2023-08-31 17:06:14 +0300

    spring-boot-starter

--- src/main/java/com/dmdev/spring/ApplicationRunner.java
+++ src/main/java/com/dmdev/spring/ApplicationRunner.java
@@ -1,18 +1,12 @@
 package com.dmdev.spring;
 
-import com.dmdev.spring.config.ApplicationConfiguration;
-import com.dmdev.spring.database.pool.ConnectionPool;
-import com.dmdev.spring.service.CompanyService;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class ApplicationRunner {
 
     public static void main(String[] args) {
-        try (var context = new AnnotationConfigApplicationContext(ApplicationConfiguration.class)) {
-            var connectionPool = context.getBean("pool1", ConnectionPool.class);
-            System.out.println(connectionPool);
-            var companyService = context.getBean(CompanyService.class);
-            System.out.println(companyService.findById(1));
-        }
+        SpringApplication.run(ApplicationRunner.class, args);
     }
 }
--- src/main/java/com/dmdev/spring/config/JPAConfiguration.java
+++ src/main/java/com/dmdev/spring/config/JPAConfiguration.java
@@ -1,9 +1,12 @@
 package com.dmdev.spring.config;
 
+import com.dmdev.spring.config.condition.JPACondition;
+import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
 
 import javax.annotation.PostConstruct;
 
+@Conditional(JPACondition.class)
 @Configuration
 public class JPAConfiguration {
 
